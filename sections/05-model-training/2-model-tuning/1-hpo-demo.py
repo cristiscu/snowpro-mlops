4e7c416d2a1c096c1f03ac831b411a716d37460f
cristiscu <cristiscu@gmail.com>
2026-06-18 06:57:41 -0700

    updates

--- sections/05-model-training/2-model-tuning/1-hpo-demo.py
+++ sections/05-model-training/2-model-tuning/1-hpo-demo.py
@@ -1,30 +1,14 @@
 from snowflake.ml.modeling import tune
-from snowflake.ml.modeling.tune.search import BayesOpt, RandomSearch, GridSearch
 
-# (1) ingest the data
-dataset_map = {
-   "train": DataConnector.from_dataframe(session.create_dataframe(X_train)),
-   "test": DataConnector.from_dataframe(session.create_dataframe(X_test)))}
-
-# (2) define search algorithm: GridSearch() / BayesOpt() / RandomSearch()
-search_alg = GridSearch()
-
-# (3) define sampling: tune.uniform/loguniform/randint/choice
-search_space = {
-   "n_estimators": tune.uniform(50, 200),
-   "max_depth": tune.uniform(3, 10),
-   "learning_rate": tune.uniform(0.01, 0.3)}
-
-# (4) configure the tunner
+# configure the tunner
 tuner_config = tune.TunerConfig(
    metric="accuracy",
    mode="max",
-   search_alg=search_algorithm.BayesOpt(
-      utility_kwargs={"kind": "ucb", "kappa": 2.5, "xi": 0.0}),
+   search_alg=search.GridSearch(),   # alt: BayesOpt() / RandomSearch()
    num_trials=5,
    max_concurrent_trials=1)
 
-# (5) get the HPs and training metrics from each training job
+# get the HPs and training metrics from each training job
 def train_func():
    tuner_context = get_tuner_context()
    config = tuner_context.get_hyper_params()
@@ -32,11 +16,21 @@ def train_func():
    ...
    tuner_context.report(metrics={"accuracy": accuracy}, model=model)
 
-# (6) init job training
-tuner = tune.Tuner(train_func, search_space, tuner_config)
-tuner_results = tuner.run(dataset_map=dataset_map)
+# init job training (w/ search space)
+tuner = tune.Tuner(
+   train_func, 
+   { "n_estimators": tune.uniform(50, 200),    # tune.loguniform/randint/choice
+      "max_depth": tune.uniform(3, 10),
+      "learning_rate": tune.uniform(0.01, 0.3)}, 
+   tuner_config)
 
-# (7) get training job results
+# ingest the data
+tuner_results = tuner.run(
+   dataset_map={
+      "train": DataConnector.from_dataframe(session.create_dataframe(X_train)),
+      "test": DataConnector.from_dataframe(session.create_dataframe(X_test)))})
+
+# get training job results
 print(tuner_results.results)
 print(tuner_results.best_model)
 print(tuner_results.best_result)
